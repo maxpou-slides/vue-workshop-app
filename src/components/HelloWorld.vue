2ebcf54644ea83c7e0257adae821fd532dd50707
Maxence POUTORD <maxence.poutord@gmail.com>
2018-01-30 18:06:03 +0000

    Step 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="ui container">
-    <div class="ui comments">
+    <div class="ui comments" v-for="question in questions" :key="question.question_id">
       <div class="comment">
         <a class="avatar">
-          <img src="https://semantic-ui.com/images/avatar/small/stevie.jpg">
+          <img :src="question.owner.profile_image">
         </a>
         <div class="content">
-          <a class="author">John Doe</a>
+          <a class="author">{{ question.owner.display_name }}</a>
           <div class="metadata">
-            <div>1 april 2017</div>
-            <div><i class="star icon"></i>1000 Votes</div>
-            <div><i class="comment icon"></i>5 answers</div>
+            <div>{{ new Date(question.creation_date*1000).toLocaleDateString() }}</div>
+            <div><i class="star icon"></i>{{ question.score }} Votes</div>
+            <div><i class="comment icon"></i>{{ question.answer_count }} answers</div>
           </div>
-          <div class="ui mini label">Vue.js</div>
+          <div v-for="tag in question.tags" :key="tag" class="ui mini label">{{ tag }}</div>
           <div class="text">
-            Hey guys, how do you bind data to the DOM?
+            {{ question.title }}
           </div>
-          <div class="actions">
-            <a href="https://stackoverflow.com/questions/1">View answers</a>
+          <div v-if="question.is_answered" class="actions">
+            <a :href="question.link">View answers</a>
           </div>
         </div>
       </div>
@@ -26,33 +26,13 @@
 </template>
 
 <script>
+import questions from '../api/stackoverflow-search-multiple.json'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      question: {
-        tags: ['javascript', 'vue.js', 'vue-resource', 'vuex'],
-        owner: {
-          reputation: 3410,
-          user_id: 2294225,
-          user_type: 'registered',
-          accept_rate: 92,
-          profile_image: 'https://i.stack.imgur.com/07yK6.jpg?s=128&g=1',
-          display_name: 'Stephan-v',
-          link: 'https://stackoverflow.com/users/2294225/stephan-v'
-        },
-        is_answered: true,
-        view_count: 5252,
-        accepted_answer_id: 38074979,
-        answer_count: 1,
-        score: 14,
-        last_activity_date: 1467114417,
-        creation_date: 1467106122,
-        last_edit_date: 1467114417,
-        question_id: 38072302,
-        link: 'https://stackoverflow.com/questions/38072302/structuring-a-vue-vuex-project',
-        title: 'Structuring a Vue + Vuex project'
-      }
+      questions: questions.items
     }
   }
 }
